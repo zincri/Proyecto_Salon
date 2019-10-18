0efc96c02d546bcfdb47a1eadd287702922f4fa9
Zincri Mendoza <zincri_mlz@hotmail.com>
2019-10-18 00:08:42 -0500

    Zincri
    Modifique nombre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Jhoana Dominguez
+                    Zincri Mendoza
                 </div>
 
                 <div class="links">
