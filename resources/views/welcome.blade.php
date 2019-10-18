29935b8d86196884e1ce5e3ecd8ec2a8c41b440d
Jhoana13 <jhoanadominguez97@gmail.com>
2019-10-18 00:04:13 -0500

    jhoana
    prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Jhoana Dominguez
                 </div>
 
                 <div class="links">
