7cf8edf2e2aa760d4108fc6969a5004a072e4409
Zincri Mendoza <zincri_mlz@hotmail.com>
2019-10-20 16:16:31 -0500

    Zincri
    Dashboard del Gerente

---
+++ app/Http/Controllers/Admin/UsuariosController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+
+class UsuariosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $datos = array(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        return view("contenido_admin.usuarios.index",['datos'=>$datos]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("contenido_admin.usuarios.create");
+    }
+
+    public function crear_usuario()
+    {
+        return "create";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+
+        //registrar en base de datos
+        return Redirect::to('administrador/usuarios');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/contenido_admin/usuarios/index.blade.php
@@ -0,0 +1,91 @@
+@extends ('layouts.master_admin')
+
+@section ('content')
+<!-- PAGE TITLE -->
+<div class="page-title">                    
+    <h2><span class="fa fa-users"></span> Lista de Usuarios <small>4 usuarios</small></h2>
+</div>
+<!-- END PAGE TITLE -->                
+
+<!-- PAGE CONTENT WRAPPER -->
+<div class="page-content-wrap">
+    
+    <div class="row">
+        <div class="col-md-12">
+            
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <p>Usa la barra buscadora para encontrar a los usuarios.</p>
+                    <div class="col-md-8">
+                    <form class="form-horizontal">
+                        <div class="form-group">
+                            <div class="col-md-12">
+                                <div class="input-group">
+                                    <div class="input-group-addon">
+                                        <span class="fa fa-search"></span>
+                                    </div>
+                                    <input type="text" class="form-control" placeholder="A quien estas buscando?"/>
+                                    <div class="input-group-btn">
+                                        <button class="btn btn-primary">Search</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form> 
+                    </div>
+                    <div class="col-md-4">
+                            <a href="{{ URL::action('Admin\UsuariosController@create')}}"><button class="btn btn-success btn-block"><span class="fa fa-plus"></span> Nuevo usuario</button></a>
+                    </div>
+                </div>
+                
+            </div>
+            
+        </div>
+    </div>
+    
+    <div class="row">
+        @foreach ($datos as $item)
+        <div class="col-md-3">
+            <!-- CONTACT ITEM -->
+            <div class="panel panel-default">
+                <div class="panel-body profile">
+                    <div class="profile-image">
+                        <img src="{{asset('assets/images/users/user3.jpg')}}" alt="Nadia Ali"/>
+                    </div>
+                    <div class="profile-data">
+                        <div class="profile-data-name">Nadia Ali</div>
+                        <div class="profile-data-title">Singer-Songwriter</div>
+                    </div>
+                    <div class="profile-controls">
+                        <a href="#" class="profile-control-left"><span class="fa fa-edit"></span></a>
+                        <a href="#" class="profile-control-right"><span class="fa fa-trash-o"></span></a>
+                    </div>
+                </div>                                
+                <div class="panel-body">                                    
+                    <div class="contact-info">
+                        <p><small>Mobile</small><br/>(555) 555-55-55</p>
+                        <p><small>Email</small><br/>nadiaali@domain.com</p>
+                        <p><small>Address</small><br/>123 45 Street San Francisco, CA, USA</p>                                   
+                    </div>
+                </div>                                
+            </div>
+            <!-- END CONTACT ITEM -->
+        </div>    
+        @endforeach                 
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <ul class="pagination pagination-sm pull-right push-down-10 push-up-10">
+                <li class="disabled"><a href="#">«</a></li>
+                <li class="active"><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>                                    
+                <li><a href="#">»</a></li>
+            </ul>                            
+        </div>
+    </div>
+
+</div>
+<!-- END PAGE CONTENT WRAPPER -->    
+@endsection
