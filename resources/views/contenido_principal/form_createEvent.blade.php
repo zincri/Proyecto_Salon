e9cc455b3247f8c050db04fecd523526ef9ee58b
Zincri Mendoza <zincri_mlz@hotmail.com>
2019-11-28 02:29:06 -0600

    Zincri
    Zincri

--- resources/views/contenido_principal/form_createEvent.blade.php
+++ resources/views/contenido_principal/form_createEvent.blade.php
@@ -33,84 +33,102 @@
 
                             <div class="col-md-6">
 
-                                {!!Form::open(array('url'=>'paquete','method'=>'POST','autocomplete'=>'off','class'=>'form-horizontal'))!!}
+
+                                {!!Form::open(array('url'=>'paquete','method'=>'POST','autocomplete'=>'off'))!!}
                                 {{Form::token()}}
+                                <div class="form-horizontal">
+                                    <div class="panel panel-default" style="background-color:burlywood;">
 
-                                <input type="text" class="form-group" style="display:none" name="paquete_id"
-                                    value="{{$datos->id}}" />
-                                <input type="text" class="form-group" style="display:none" name="url_imagen_principal"
-                                    value="{{$datos->foto_principal}}" />
 
-                                <div class="form-group {{$errors->has('descripcion') ? 'has-error':''}}">
-                                    <label class="col-md-3 control-label">Descripcion del Paquete</label>
-                                    <div class="col-md-9">
-                                        <div class="input-group">
-                                            <span class="input-group-addon"><span class="fa fa-pencil"></span></span>
-                                            <input type="text" class="form-control" name="descripcion"
-                                                value="{{old('descripcion')}}" />
+                                        <div class="panel-body">
+                                            <input type="text" class="form-group" style="display:none" name="paquete_id"
+                                                value="{{$datos->id}}" />
+                                            <input type="text" class="form-group" style="display:none"
+                                                name="url_imagen_principal" value="{{$datos->foto_principal}}" />
+
+                                            <div class="form-group {{$errors->has('descripcion') ? 'has-error':''}}">
+                                                <label class="col-md-3 control-label">Descripcion del Paquete</label>
+                                                <div class="col-md-9">
+                                                    <div class="input-group">
+                                                        <span class="input-group-addon"><span
+                                                                class="fa fa-pencil"></span></span>
+                                                        <input type="text" class="form-control" name="descripcion"
+                                                            value="{{old('descripcion')}}" />
+                                                    </div>
+                                                    {!! $errors->first('descripcion','<span
+                                                        class="help-block">:message</span>')!!}
+
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group {{$errors->has('nombre') ? 'has-error':''}}">
+                                                <label class="col-md-3 control-label">Nombre del Paquete</label>
+                                                <div class="col-md-9">
+                                                    <div class="input-group">
+                                                        <span class="input-group-addon"><span
+                                                                class="fa fa-pencil"></span></span>
+                                                        <input type="text" class="form-control" name="nombre"
+                                                            value="{{old('nombre')}}" />
+                                                    </div>
+                                                    {!! $errors->first('nombre','<span
+                                                        class="help-block">:message</span>')!!}
+
+                                                </div>
+                                            </div>
+
+                                            <div
+                                                class="form-group {{$errors->has('numero_invitados') ? 'has-error':''}}">
+                                                <label class="col-md-3 control-label">Numero de invitados</label>
+                                                <div class="col-md-9">
+                                                    <div class="input-group">
+                                                        <span class="input-group-addon"><span
+                                                                class="fa fa-pencil"></span></span>
+                                                        <input type="text" class="form-control" name="numero_invitados"
+                                                            value="{{old('numero_invitados')}}" />
+                                                    </div>
+                                                    {!! $errors->first('numero_invitados','<span
+                                                        class="help-block">:message</span>')!!}
+
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-md-3 control-label">Fecha</label>
+                                                <div class="col-md-9">
+                                                    <div class="input-group">
+
+                                                        <span class="input-group-addon"><span
+                                                                class="fa fa-calendar"></span></span>
+                                                        <input type="text" class="form-control datepicker"
+                                                            value="2019-12-01" data-date="01-12-2019"
+                                                            data-date-format="dd-mm-yyyy" data-date-viewmode="years"
+                                                            name="fecha">
+                                                    </div>
+                                                </div>
+                                            </div>
+
+
+                                            <div class="form-group">
+                                                <label class="col-md-3 control-label">Hora</label>
+                                                <div class="col-md-9">
+                                                    <div class="input-group bootstrap-timepicker">
+                                                        <span class="input-group-addon"><span
+                                                                class="glyphicon glyphicon-time"></span></span>
+                                                        <input type="text"
+                                                            class="form-control timepicker" name="hora" value="8:00 PM" />
+
+                                                    </div>
+                                                </div>
+                                            </div>
+
+
+
+                                            <div class="panel-footer" style="background-color:burlywood;">
+                                                <button class="btn btn-primary pull-right" type="submit">Crear
+                                                    Evento</button>
+                                            </div>
                                         </div>
-                                        {!! $errors->first('descripcion','<span class="help-block">:message</span>')!!}
-
                                     </div>
-                                </div>
-
-                                <div class="form-group {{$errors->has('nombre') ? 'has-error':''}}">
-                                    <label class="col-md-3 control-label">Nombre del Paquete</label>
-                                    <div class="col-md-9">
-                                        <div class="input-group">
-                                            <span class="input-group-addon"><span class="fa fa-pencil"></span></span>
-                                            <input type="text" class="form-control" name="nombre"
-                                                value="{{old('nombre')}}" />
-                                        </div>
-                                        {!! $errors->first('nombre','<span class="help-block">:message</span>')!!}
-
-                                    </div>
-                                </div>
-
-                                <div class="form-group {{$errors->has('numero_invitados') ? 'has-error':''}}">
-                                    <label class="col-md-3 control-label">Numero de invitados</label>
-                                    <div class="col-md-9">
-                                        <div class="input-group">
-                                            <span class="input-group-addon"><span class="fa fa-pencil"></span></span>
-                                            <input type="text" class="form-control" name="numero_invitados"
-                                                value="{{old('numero_invitados')}}" />
-                                        </div>
-                                        {!! $errors->first('numero_invitados','<span
-                                            class="help-block">:message</span>')!!}
-
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="col-md-3 control-label">Fecha</label>
-                                    <div class="col-md-9">
-                                        <div class="input-group">
-
-                                                <span class="input-group-addon"><span class="fa fa-calendar"></span></span>
-                                            <input type="text" class="form-control datepicker" value="2014-11-01"
-                                                data-date="06-06-2014" data-date-format="dd-mm-yyyy"
-                                                data-date-viewmode="years" name="fecha">
-                                        </div>
-                                    </div>
-                                </div>
-
-
-
-                                <div class="form-group">
-                                    <label class="col-md-3 control-label">Hora</label>
-                                    <div class="col-md-9">
-                                        <div class="input-group bootstrap-timepicker">
-                                                <span class="input-group-addon"><span
-                                                    class="glyphicon glyphicon-time"></span></span>
-                                            <input type="text" class="form-control timepicker" name="hora" />
-                                            
-                                        </div>
-                                    </div>
-                                </div>
-
-
-                                <div class="panel-footer" style="background-color:burlywood;">
-                                    <button class="btn btn-primary pull-right" type="submit">Crear Evento</button>
                                 </div>
                                 {!!Form::close()!!}
 
--- resources/views/layouts/master_principal.blade.php
+++ resources/views/layouts/master_principal.blade.php
@@ -33,11 +33,7 @@
 	<link href="{{ asset ('css/style2.css') }}" rel="stylesheet">
 
 
-	<!-- START PLUGINS -->
-	<script type="text/javascript" src="{{ asset ('js/plugins/jquery/jquery.min.js') }}"></script>
-	<script type="text/javascript" src="{{ asset ('js/plugins/jquery/jquery-ui.min.js') }}"></script>
-	<script type="text/javascript" src="{{ asset ('js/plugins/bootstrap/bootstrap.min.js') }}"></script>
-	<!-- END PLUGINS -->
+	
 
 	
 
@@ -113,9 +109,11 @@
 
 
 
-
-	<script src="{{asset ('https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places')}}"></script>
-
+<!-- START PLUGINS -->
+<script type="text/javascript" src="{{ asset ('js/plugins/jquery/jquery.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset ('js/plugins/jquery/jquery-ui.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset ('js/plugins/bootstrap/bootstrap.min.js') }}"></script>
+<!-- END PLUGINS -->
 	<script type='text/javascript' src="{{ asset ('js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
 	<script type='text/javascript' src="{{ asset ('js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
 	<script type='text/javascript' src="{{ asset ('js/plugins/jvectormap/jquery-jvectormap-europe-mill-en.js')}}"></script>
@@ -138,35 +136,32 @@
 
 	<script src="{{ asset ('js/simplyCountdown.js') }}"></script>
 
-	<script type="text/javascript" src="{{asset('js/plugins/bootstrap/bootstrap-datepicker.js')}}"></script>
-
-	<script type='text/javascript' src="{{ asset('js/plugins/bootstrap/bootstrap-timepicker.min.js')}}"></script>                
 	
-	<script type='text/javascript' src="{{ asset('js/plugins/daterangepicker/daterangepicker.js')}}"></script>     
 
 	<!-- Main -->
 	<script src="{{ asset ('js/main.js') }}"></script>
 
 	
 
-	<script type="text/javascript" src="{{asset('js/plugins/bootstrap/bootstrap-colorpicker.js')}}"></script>
-	
-	<!-- END THIS PAGE PLUGINS -->
 	<!-- THIS PAGE PLUGINS -->
 	<script type='text/javascript' src='{{asset('js/plugins/icheck/icheck.min.js')}}'></script>
 	<script type="text/javascript" src="{{asset('js/plugins/mcustomscrollbar/jquery.mCustomScrollbar.min.js')}}"></script>
+
+	<script type='text/javascript' src="{{ asset('js/plugins/bootstrap/bootstrap-datepicker.js')}}"></script>                
+        
+	<script type='text/javascript' src="{{ asset('js/plugins/bootstrap/bootstrap-timepicker.min.js')}}"></script>                
+	<script type='text/javascript' src="{{ asset('js/plugins/daterangepicker/daterangepicker.js')}}"></script>                
 	
-	<script type="text/javascript" src="{{asset('js/plugins/bootstrap/bootstrap-datepicker.js')}}"></script>
-	<script type="text/javascript" src="{{asset('js/plugins/bootstrap/bootstrap-timepicker.min.js')}}"></script>
-	<script type="text/javascript" src="{{asset('js/plugins/bootstrap/bootstrap-colorpicker.js')}}"></script>
-	<script type="text/javascript" src="{{asset('js/plugins/bootstrap/bootstrap-file-input.js')}}"></script>
-	<script type="text/javascript" src="{{asset('js/plugins/bootstrap/bootstrap-select.js')}}"></script>
-	<script type="text/javascript" src="{{asset('js/plugins/tagsinput/jquery.tagsinput.min.js')}}"></script>
+	<script type="text/javascript" src="{{ asset('js/plugins/owl/owl.carousel.min.js')}}"></script>                 
+	
+	<script type="text/javascript" src="{{ asset('js/plugins/moment.min.js')}}"></script>
+	<script type="text/javascript" src="{{ asset('js/plugins/daterangepicker/daterangepicker.js')}}"></script>
 	<!-- END THIS PAGE PLUGINS -->   
+
 
 	<script type="text/javascript" src="{{ asset('js/plugins.js')}}"></script>        
 	<script type="text/javascript" src="{{ asset('js/actions.js')}}"></script>
-
+	<script type="text/javascript" src="{{ asset('js/demo_dashboard.js')}}"></script>
 </body>
 
 </html>
